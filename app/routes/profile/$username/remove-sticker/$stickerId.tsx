a1a58e03def6e0b359d216e60d811de1e1f1c0d9
Jack Arthur Harrhy <me@jackharrhy.com>
2022-04-13 01:33:20 -0230

    more proper sticker deletion

--- app/routes/profile/$username/remove-sticker/$stickerId.tsx
+++ app/routes/profile/$username/remove-sticker/$stickerId.tsx
@@ -2,28 +2,53 @@ import { ActionFunction, Form, redirect, useNavigate } from "remix";
 import invariant from "tiny-invariant";
 import { Modal } from "~/components/Modal";
 import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/session.server";
 
-export const action: ActionFunction = async ({ params }) => {
-  invariant(params.stickerId, "expected params.username");
+export const action: ActionFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
 
-  const deletedSticker = await db.sticker.delete({
+  if (!userId) {
+    return redirect("/login");
+  }
+
+  invariant(params.stickerId, "expected params.stickerId");
+
+  const sticker = await db.sticker.findUnique({
     where: { id: params.stickerId },
     select: {
       owner: {
         select: {
+          id: true,
           username: true,
         },
       },
     },
   });
 
+  if (sticker === null) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
+  }
+
+  if (sticker.owner === null) {
+    // TODO admins should be able to cleanup these stickers
+    throw new Error("Attempt to delete a sticker without an owner");
+  }
+
+  if (userId !== sticker.owner.id) {
+    throw new Response("Forbidden", {
+      status: 403,
+    });
+  }
+
+  await db.sticker.delete({
+    where: { id: params.stickerId },
+  });
+
   // TODO cleanup assets within minio as well
 
-  if (deletedSticker.owner === null) {
-    return redirect("/");
-  } else {
-    return redirect(`/profile/${deletedSticker.owner.username}`);
-  }
+  return redirect(`/profile/${sticker.owner.username}`);
 };
 
 export default function RemoveSticker() {
